c3b6065c19ae26851068be3eaf26fe0c719030f0
Callan Barrett <callan@zoocar.org>
2014-11-22 00:24:58 +0800

    Shitloads of stuff
    Methods for changing all player attrs, player tests, dealer method, comments

--- dopewars/dopewars.py
+++ dopewars/dopewars.py
@@ -1,6 +1,8 @@
 # dope wars
 
-drugs = {
+import random
+
+DRUGS = {
     "weed": {
         "name": "Weed",
         "base_price": 100
@@ -21,6 +23,16 @@ class Player():
             "drugs": {}
         }
 
+        # TODO: weapons
+
+    # prettu printing
+    def print_trenchcoat(self):
+        print("Max: %s" % (self.trenchcoat["max"]))
+        for k, v in self.trenchcoat["drugs"].items():
+            print("%s x%i ($%f)" % (DRUGS[k]["name"],
+                                    v["count"],
+                                    v["price"]))
+
     def print_status(self):
         template = "%s\nHP: %i/%i\nCash: $%f\nLoan: $%f\nBank: $%f"
         print(template % (self.name,
@@ -29,9 +41,10 @@ class Player():
                           self.loan,
                           self.bank))
 
-    def save(selfa):
+    # save/load
+    def save(self):
         """Return entire player instance as a map."""
-        {
+        return {
             "name": self.name,
             "health": self.health,
             "cash": self.cash,
@@ -74,10 +87,83 @@ class Player():
         self.cash += amount
 
     def spend_cash(self, amount):
-        """If player has enough cash, deduct from total and return True, else False."""
+        """If player has enough cash, deduct from total. False if not enough."""
         # can't spend more than you have
         if self.cash >= amount:
             self.cash -= amount
+            return True
+        else:
+            return False
+
+    # drugs
+    def total_drugs(self):
+        """Return total number of all drugs in trenchcoat."""
+        total = 0
+        for drug in self.trenchcoat["drugs"].values():
+            total += drug["count"]
+        return total
+
+    def add_drug(self, name, price, count):
+        """Add any drug to trenchcoat at no cost, False if not enough space."""
+        # check if there's enough space
+        if count + self.total_drugs() > self.trenchcoat["max"]:
+            return False
+
+        if name in self.trenchcoat["drugs"].keys():
+            count += self.trenchcoat["drugs"][name]["count"]
+
+        self.trenchcoat["drugs"][name] = {
+            "price": price,
+            "count": count
+        }
+
+        return True
+
+    def buy_drug(self, name, price, count):
+        """Add any drug to trenchcoat at cost, False if not enough space/cash."""
+        if self.spend_cash(price * count):
+            self.add_drug(name, price, count)
+            return True
+        else:
+            return False
+
+    def remove_drug(self, name, count):
+        """Remove number of drug from trenchcoat, False if too many."""
+        if name not in self.trenchcoat["drugs"].keys():
+            return False
+
+        new_count = self.trenchcoat["drugs"][name]["count"] - count
+        if new_count > 0:
+            self.trenchcoat["drugs"][name]["count"] = new_count
+            return True
+        elif new_count == 0:
+            self.trenchcoat["drugs"].pop(name)
+            return True
+        else:
+            return False
+
+    # loans
+    def add_loan(self, amount):
+        """Add amount to player loan."""
+        self.loan += amount
+
+    def remove_loan(self, amount):
+        """Remove amount from player loan, False if too much."""
+        if amount <= self.loan:
+            self.loan -= amount
+            return True
+        else:
+            return False
+
+    # bank
+    def add_bank(self, amount):
+        """Add amount to player bank."""
+        self.bank += amount
+
+    def remove_bank(self, amount):
+        """Remove amount from player bank, False if too much."""
+        if amount <= self.bank:
+            self.bank -= amount
             return True
         else:
             return False
@@ -92,15 +178,35 @@ class World():
         self.events = []
 
         self.player = None
+        self.dealer = None
 
     def new_world(self, player_name, max_days=30):
         """Create a brand new game."""
         self.day[1] = max_days
+
         self.player = Player()
         self.player.name = player_name
 
+        self.new_dealer()
+
     # TODO: load world
 
+    # dealer
+    def new_dealer(self):
+        self.dealer = {}
+        for k, v in DRUGS.items():
+            # TODO: shouldn't be harcoded (or so simple)
+            self.dealer[k] = random.randint(1, 3) * v["base_price"]
+
+    def buy_from_dealer(self, name, count=1):
+        # check they actually have the drug
+        if name in self.dealer.keys():
+            price = self.dealer[name]
+            return self.player.buy_drug(name, price, count)
+        else:
+            return False
+
+    # events
     def add_event(self, name, wait=0):
         """Queue up a new event."""
         self.events.append({"name": name, "wait": wait})
--- dopewars/dopewars_tests.py
+++ dopewars/dopewars_tests.py
@@ -1,17 +1,28 @@
 import unittest
 
-from dopewars import *
+#from dopewars import *
 
 class TestPlayer(unittest.TestCase):
     def setUp(self):
+        self.player = Player()
+
+    def reset_player(self):
         self.player = Player()
 
     def test_new(self):
         self.assertEqual(self.player.name, "John Cena")
         self.player.name = "Test Guy"
         self.assertEqual(self.player.name, "Test Guy")
+        self.assertEqual(self.player.health, [100, 100])
+        self.assertEqual(self.player.cash, 0)
+        self.assertEqual(self.player.loan, 1000)
+        self.assertEqual(self.player.bank, 0)
+        self.assertEqual(self.player.trenchcoat, {"max":100,"drugs":{}})
+
+    # TODO: save/load
 
     def test_health(self):
+        self.reset_player()
         self.assertEqual(self.player.health, [100, 100])
         self.player.damage(10)
         self.assertEqual(self.player.health[0], 90)
@@ -25,6 +36,7 @@ class TestPlayer(unittest.TestCase):
         self.assertEqual(self.player.health, [100, 100])
 
     def test_cash(self):
+        self.reset_player()
         self.assertEqual(self.player.cash, 0)
         self.player.add_cash(100)
         self.assertEqual(self.player.cash, 100)
@@ -32,3 +44,48 @@ class TestPlayer(unittest.TestCase):
         self.assertEqual(self.player.cash, 100)
         self.assertTrue(self.player.spend_cash(100))
         self.assertEqual(self.player.cash, 0)
+
+    def test_drugs(self):
+        self.reset_player()
+        self.assertEqual(self.player.total_drugs(), 0)
+        self.assertTrue(self.player.add_drug("weed",0,1))
+        self.assertEqual(self.player.total_drugs(), 1)
+        self.assertFalse(self.player.add_drug("weed",0,1000))
+        self.assertEqual(self.player.total_drugs(), 1)
+        self.assertTrue(self.player.add_drug("weed",0,1))
+        self.assertEqual(self.player.total_drugs(), 2)
+        self.assertEqual(self.player.trenchcoat["drugs"]["weed"]["count"], 2)
+        self.assertFalse(self.player.buy_drug("weed",100,1))
+        self.player.add_cash(100)
+        self.assertTrue(self.player.buy_drug("weed",100,1))
+        self.assertFalse(self.player.remove_drug("weed",100))
+        self.assertEqual(self.player.total_drugs(), 3)
+        self.assertTrue(self.player.remove_drug("weed",1))
+        self.assertEqual(self.player.total_drugs(), 2)
+        self.assertTrue(self.player.remove_drug("weed",2))
+        self.assertEqual(self.player.total_drugs(), 0)
+        self.assertFalse(self.player.trenchcoat["drugs"].pop("weed",False))
+
+    def test_loan(self):
+        self.reset_player()
+        self.assertEqual(self.player.loan, 1000)
+        self.player.add_loan(1000)
+        self.assertEqual(self.player.loan, 2000)
+        self.assertTrue(self.player.remove_loan(500))
+        self.assertEqual(self.player.loan, 1500)
+        self.assertFalse(self.player.remove_loan(2000))
+        self.assertEqual(self.player.loan, 1500)
+        self.assertTrue(self.player.remove_loan(1500))
+        self.assertEqual(self.player.loan, 0)
+
+    def test_bank(self):
+        self.reset_player()
+        self.assertEqual(self.player.bank, 0)
+        self.player.add_bank(1000)
+        self.assertEqual(self.player.bank, 1000)
+        self.assertTrue(self.player.remove_bank(500))
+        self.assertEqual(self.player.bank, 500)
+        self.assertFalse(self.player.remove_bank(1000))
+        self.assertEqual(self.player.bank, 500)
+        self.assertTrue(self.player.remove_bank(500))
+        self.assertEqual(self.player.bank, 0)
